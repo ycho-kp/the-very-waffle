407fc7760f8b53a14d3ea9e0f94ab711d5fd5543
ycho-kp <plplkj16@naver.com>
2023-05-28 22:47:30 +0900

    test

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = 'ycho_analyze',
+    version = '0.0.1',
+    description = 'ycho data analyze library',
+    url = 'https://github.com/ycho-kp/the-very-waffle',
+    author = 'choyoungju',
+    author_email = 'plplkj16@korea.ac.kr',
+    license = 'ycho',
+    packages = ['ycho_analyze'],
+    zip_safe = False,
+    install_requires = [
+        'numpy == 1.18.3'
+    ]
+)
---
+++ ycho_analyze/ycho.py
@@ -0,0 +1,2 @@
+def print_ycho(msg):
+    print(f"안녕하세요? 조영주입니다. {msg}")
